3a6eab136a21f9aa6d8c6fdbeb755babe05d931c
Heena Sood <soodheena0001@gmail.com>
2020-07-10 18:14:19 -0700

    Second Commit

---
+++ weather.js
@@ -0,0 +1,52 @@
+
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+function getResults (query) {
+var api='https://openweathermap.org/data/2.5/weather?q=';
+	var key='&appid=439d4b804bc8187953eb36d2a8c26a02';
+var url= api+query+key;
+console.log(url);
+
+  fetch(url)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = `${weather.weather[0].main}, ( ${weather.weather[0].description} )`;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${weather.main.temp_min} °c / ${weather.main.feels_like}°c`;
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
